66bab9251d9353e5a4db2116359fe4262b827a5a
cjalex1313 <cioarec.alexandru96@gmail.com>
2018-06-30 18:24:25 +0300

    no message

---
+++ client/src/components/Start.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Start page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Start'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
